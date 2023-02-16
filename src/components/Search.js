78c3066dde0432df5f05d47c3b7b05c9b84d366d
ottakist <102596775+ottakist@users.noreply.github.com>
2023-02-16 23:53:33 +0200

    search finished

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const user = React.useRef('');
-  const { getUser } = React.useContext(GithubContext);
+  const { getUser, requests,error,isLoading } = React.useContext(GithubContext);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (user) {
@@ -14,14 +14,19 @@ const Search = () => {
   return (
     <section className='section'>
       <Wrapper className='section-center'>
+        {error.show && 
+        <ErrorWrapper>
+        <p>{error.msg}</p>
+        </ErrorWrapper>
+        }
         <form onSubmit={handleSubmit}>
           <div className='form-control'>
             <MdSearch />
             <input type='text' placeholder='enter github user' ref={user} />
-            <button type='submit'>search</button>
+            {requests > 0 &&!isLoading && <button type='submit'>search</button>}
           </div>
         </form>
-        <h3>requests : 60 / 60</h3>
+        <h3>requests : {requests} / 60</h3>
       </Wrapper>
     </section>
   );
--- src/context/context.js
+++ src/context/context.js
@@ -10,45 +10,68 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
-  const [requests, setRequests] = useState(0)
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState({show:false,msg:""})
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: '' });
+
   const getUser = async (user) => {
     toggleError()
-      const response = await axios
-        .get(`${rootUrl}/users/${user}`)
-        .catch((err) => console.log(err));
-      setGithubUser(response.data);
-      getRepos(response.data.repos_url);
-      getFollowers(response.data.followers_url);
-  
+    setIsLoading(true);
+    const response = await axios
+      .get(`${rootUrl}/users/${user}`)
+      .catch((err) => console.log(err));
+      if(response){
+
+        setGithubUser(response.data);
+        getRepos(response.data.repos_url);
+        getFollowers(response.data.followers_url);
+      }
+      else{
+        toggleError(true,"there is no user whit that username")
+      }
+      checkRequests()
+      setIsLoading(false)
   };
+
   const getRepos = async (reposLink) => {
-
-      const response = await axios
-        .get(reposLink)
-        .catch((err) => console.log(err));
-      setRepos(response.data);
-      console.log(response);
-  
+    const response = await axios
+      .get(`${reposLink}?per_page=100`)
+      .catch((err) => console.log(err));
+    setRepos(response.data);
+    
   };
+
   const getFollowers = async (followersLink) => {
-    
-      const response = await axios
-        .get(followersLink)
-        .catch((err) => console.log(err));
+    const response = await axios
+      .get(`${followersLink}?per_page=100`)
+      .catch((err) => console.log(err));
 
-      setFollowers(response.data);
-    
+    setFollowers(response.data);
   };
-   function toggleError(show = false, msg = '') {
-     setError({ show, msg });
-   }
-   useEffect(()=>{
-    getUser("ottakist")
-   },[])
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining }
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, 'sorry, you have exceeded your hourly rate limit!');
+        }
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg });
+  }
+  useEffect(checkRequests,[])
+  useEffect(() => {
+    getUser('ottakist');
+  }, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers, getUser }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers, getUser,requests,error,isLoading}}>
       {children}
     </GithubContext.Provider>
   );
