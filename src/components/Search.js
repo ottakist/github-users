882b800a7fdd5cfd5a045e54a072535535ebea22
ottakist <102596775+ottakist@users.noreply.github.com>
2023-02-16 22:58:52 +0200

    request logic

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,28 @@ import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const user = React.useRef('');
+  const { getUser } = React.useContext(GithubContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user) {
+      getUser(user.current.value);
+    }
+  };
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <MdSearch />
+            <input type='text' placeholder='enter github user' ref={user} />
+            <button type='submit'>search</button>
+          </div>
+        </form>
+        <h3>requests : 60 / 60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -7,8 +7,50 @@ import axios from 'axios';
 const rootUrl = 'https://api.github.com';
 const GithubContext = React.createContext();
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState({show:false,msg:""})
+  const getUser = async (user) => {
+    toggleError()
+      const response = await axios
+        .get(`${rootUrl}/users/${user}`)
+        .catch((err) => console.log(err));
+      setGithubUser(response.data);
+      getRepos(response.data.repos_url);
+      getFollowers(response.data.followers_url);
+  
+  };
+  const getRepos = async (reposLink) => {
+
+      const response = await axios
+        .get(reposLink)
+        .catch((err) => console.log(err));
+      setRepos(response.data);
+      console.log(response);
+  
+  };
+  const getFollowers = async (followersLink) => {
+    
+      const response = await axios
+        .get(followersLink)
+        .catch((err) => console.log(err));
+
+      setFollowers(response.data);
+    
+  };
+   function toggleError(show = false, msg = '') {
+     setError({ show, msg });
+   }
+   useEffect(()=>{
+    getUser("ottakist")
+   },[])
   return (
-    <GithubContext.Provider value={{}}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers, getUser }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
-export {GithubContext,GithubProvider}
+export { GithubContext, GithubProvider };
